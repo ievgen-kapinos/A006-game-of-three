3e3534d9431bb9713f4adbe47307a8ef37c241d6
Ievgen Kapinos <ievgen.kapinos@gmail.com>
2020-12-18 15:29:52 +0200

    Init Maven project. Ignore IDE artefacts

---
+++ src/main/java/local/kapinos/App.java
@@ -0,0 +1,13 @@
+package local.kapinos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
