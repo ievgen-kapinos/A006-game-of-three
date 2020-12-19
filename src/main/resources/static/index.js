2e7bc4fd2a7929049d298b7a5b1bb19a6eba5dcf
Ievgen Kapinos <ievgen.kapinos@gmail.com>
2020-12-19 21:37:56 +0200

    Added Ajax to server

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,35 @@
-const userId = Math.floor(Math.random() * 1_000_000_000);
+
+function startGame(palyerId) {	
+	var numberControl = $("#player" + palyerId + "-number");
+	
+	if (!numberControl[0].reportValidity()) {
+		return;
+	}
+	
+	var gameId = Math.floor(Math.random() * 1000000);
+	
+	$.ajax({
+	  method: "POST",
+	  url: "start-game",
+	  data: { 
+		  gameId: gameId,
+		  playerId: palyerId,
+		  number: numberControl.val() 
+	  }
+	});
+}
 
 $(function () {
-    $("#userId").text(userId);
+    // TODO Inital statuses should be provided via Thymeleaf 
+    
+	// TODO need proper handling
+    $("#player1-status,#player2-status")
+    	.removeClass('badge-secondary')
+    	.addClass('badge-success')
+    	.text("Active");
+    
+    $("#player1-startGame,#player2-startGame").prop("disabled", false); 
+    
+    $("#player1-startGame").click(function() { startGame(1); });
+    $("#player2-startGame").click(function() { startGame(2); });
 });
